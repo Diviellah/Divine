37a8060b535ee459432901bb3d89b207491661e3
Diviellah <divineshyla2@gmail.com>
2024-09-10 22:20:13 +0300

    Update VehicleTest.java

--- Divine/src/ccom/vehicle/test/VehicleTest.java
+++ Divine/src/ccom/vehicle/test/VehicleTest.java
@@ -3,14 +3,16 @@ package ccom.vehicle.test;
  import com.vehicle.Electriccar;
 public class VehicleTest {
     public static void main(String[] args) {
-        car carone = new car();
- carone.setSpeed(120);/* i used the implicit method to convert the String speed to a double speed */
+        car carone = new car();/*Used the explicit method for a method call*/
+     
+ carone.setSpeed(120);/*Used the implicit conversion changing speed from a method that returns to double */
  double speed= carone.getSpeedMph();
- String speed_String =Double.toString(speed);
+ String speed_String =Double.toString(speed);/* Explicit conversion from double to String*/
+     
  carone.setFuelLevel(78.0f);
  carone.drive(33270);
    System.out.println("speed in mph is "+" "+ carone.getSpeedMph());
-   System.out.println("distance traveled is "+" "+ carone.getDistanceKm());
+   System.out.println("distance traveled is "+" "+ carone.getDistanceKm());/*Implicit conversion to a String*/
    System.out.println("remaining fuel percentage is"+" "+ carone.calculateRemainingFuel(109));
 
    Electriccar econe=new Electriccar();
